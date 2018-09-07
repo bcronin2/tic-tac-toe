cd2ae3a234c91ec7505c340781fe4fc70cbb4c0f
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-09-06 17:32:21 -0700

    Get gameplay working

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-const { makeBoard } = require('./board.js');
-const { takeTurn } = require('./game.js');
-const { prompt } = require('./utils.js');
-
-const displayMessage = console.log;
+const { prompt, displayMessage, makeBoard, drawBoard } = require('./utils.js');
 
 const initializeSession = () => {
   displayMessage('TIC-TAC-TOE');
   displayMessage('-----------');
-  prompt('Player 1 (X) name:', name => {
-    const player1 = name + ' (X)' || 'X';
-    prompt('Player 2 (O) name:', name => {
-      const player2 = name + '(O)' || 'O';
+  prompt('Player 1 (X) name', name => {
+    const player1 = name ? name + ' (X)' : 'X';
+    prompt('Player 2 (O) name', name => {
+      const player2 = name ? name + ' (O)' : 'O';
       prompt('Board dimension', dimension => {
         const boardDimension = parseInt(dimension) || 3;
         playGame(player1, player2, boardDimension);
@@ -19,11 +15,22 @@ const initializeSession = () => {
   });
 };
 
+const endSession = () => {
+  displayMessage('Goodbye!');
+};
+
 const playGame = (player1, player2, boardDimension) => {
   const board = makeBoard(boardDimension);
-  const winner = takeTurn('X', 0, board, player1, player2);
+  takeTurn('X', 0, board, player1, player2);
+};
+
+const finishGame = winner => {
   const message = winner ? `${winner} won!` : 'The game was a draw!';
   displayMessage(message);
+  playAgain(player1, player2, boardDimension);
+};
+
+const playAgain = (player1, player2, boardDimension) => {
   prompt('Play again? (y/n)', playAgain => {
     if (playAgain !== 'n' && playAgain !== 'N') {
       playGame(player1, player2, boardDimension);
@@ -33,8 +40,62 @@ const playGame = (player1, player2, boardDimension) => {
   });
 };
 
-const endSession = () => {
-  displayMessage('Goodbye!');
+const takeTurn = (currentTurn, turnCounter, board, player1, player2) => {
+  const currentPlayer = currentTurn === 'X' ? player1 : player2;
+  displayMessage(`${currentPlayer}'s turn!`);
+  displayMessage(drawBoard(board));
+  makeMove(currentTurn, turnCounter, board, player1, player2);
+};
+
+const makeMove = (currentTurn, turnCounter, board, player1, player2) => {
+  displayMessage('Enter a valid position for your move.');
+  prompt(`Row number (1-${board.length})`, result => {
+    const row = parseInt(result);
+    prompt(`Column number (1-${board[0].length})`, result => {
+      const col = parseInt(result);
+      if (!validateCoordinates(row, col, board)) {
+        makeMove(currentTurn, turnCounter, board, player1, player2);
+      } else {
+        board[row - 1][col - 1] = currentTurn;
+        endTurn(currentTurn, turnCounter, board, player1, player2, row, col);
+      }
+    });
+  });
+};
+
+const validateCoordinates = (row, col, board) => {
+  const isRowValid = row >= 1 && row <= board.length;
+  const isColValid = col >= 1 && col <= board[0].length;
+  return isRowValid && isColValid && !board[row][col];
+};
+
+const endTurn = (currentTurn, turnCounter, board, player1, player2, row, col) => {
+  const currentPlayer = currentTurn === 'X' ? player1 : player2;
+  if (checkWinner(board, row, col)) {
+    displayMessage(drawBoard(board));
+    finishGame(currentPlayer);
+  }
+  if (++turnCounter === board.length * board[0].length) {
+    displayMessage(drawBoard(board));
+    finishGame(null);
+  }
+  const nextTurn = currentTurn === 'X' ? 'O' : 'X';
+  takeTurn(nextTurn, turnCounter, board, player1, player2);
+};
+
+const checkWinner = (board, row, col) => {
+  const currentVal = board[row - 1][col - 1];
+  const isRowWinner = board[row].reduce((allMatch, current) => allMatch && current === currentVal, true);
+  const isColWinner = board.reduce((allMatch, current) => allMatch && current[col] === currentVal, true);
+  const isMajWinner = board.reduce(
+    (allMatch, current) => allMatch && current[col + (current - row)] === currentVal,
+    true
+  );
+  const isMinWinner = board.reduce(
+    (allMatch, current) => allMatch && current[col + (row - current)] === currentVal,
+    true
+  );
+  return isRowWinner || isColWinner || isMajWinner || isMinWinner;
 };
 
 initializeSession();
--- utils.js
+++ utils.js
@@ -1,8 +1,34 @@
 const promptUtil = require('prompt');
 
+const displayMessage = console.log;
+
 const prompt = (message, cb) => {
   promptUtil.start();
   promptUtil.get([message], (err, result) => cb(result[message]));
 };
 
-module.exports = { prompt };
+const makeBoard = boardDimension => {
+  const board = [];
+  for (let i = 0; i < boardDimension; i++) {
+    board.push([]);
+    for (let j = 0; j < boardDimension; j++) {
+      board[i].push(null);
+    }
+  }
+  return board;
+};
+
+const drawBoard = board => {
+  let boardString = '\n';
+  for (let i = 0; i < board.length; i++) {
+    boardString += '| ';
+    for (let j = 0; j < board[i].length; j++) {
+      boardString += board[i][j] ? board[i][j] : ' ';
+      boardString += ' | ';
+    }
+    boardString += '\n';
+  }
+  return boardString;
+};
+
+module.exports = { prompt, displayMessage, makeBoard, drawBoard };
